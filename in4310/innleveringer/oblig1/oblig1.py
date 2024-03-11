e53dbc246cc20e6291804a16ef8c27a27ba6d21b
Adrian Duric <adria.dur@gmail.com>
2024-03-11 14:03:16 +0100

    add stuff

--- in4310/innleveringer/oblig1/oblig1.py
+++ in4310/innleveringer/oblig1/oblig1.py
@@ -140,6 +140,7 @@ def test_model(model, dataloader):
             batch_labels = batch_labels.to("cuda")
         with torch.no_grad():
             batch_predictions = model(batch_images)
+            batch_images = nn.functional.one_hot(batch_images)
             all_predictions = torch.cat((all_predictions, batch_predictions), 0)
             all_labels = torch.cat((all_labels, batch_labels), 0)
 
